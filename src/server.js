e6ca6ebf60ca6f672ca4fdc13f5b5788d60d3ade
Gianni Cafano <gcafano@biuk.bi.corp>
2016-12-02 11:14:53 +0000

    Added routes on express server

--- src/server.js
+++ src/server.js
@@ -6,6 +6,21 @@ const port = 3000;
 
 server.use(express.static(__dirname));
 
+server.get('/allFiles', function(req, res){
+	console.log("Retrieved files");
+
+});
+
+server.post('/newFile', function(req, res){
+	console.log("posted file(s)");
+
+});
+
+server.delete('/deleteFile', function(req, res) {
+	console.log("deleted file");
+
+});
+
 server.listen(port);
 
 console.log('Server listening at localhost change: ' + port + '/');
